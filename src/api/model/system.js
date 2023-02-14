8cb444a544585db3181122d5fb8d44a00f2a628f
xiazunjun <xiaj07@chinatelecom.cn>
2023-02-14 09:04:47 +0800

    初始化

---
+++ src/api/model/system.js
@@ -0,0 +1,163 @@
+import config from "@/config"
+import http from "@/utils/request"
+
+export default {
+	menu: {
+		myMenus: {
+			url: `${config.API_URL}/system/menu/my/1.6.1`,
+			name: "获取我的菜单",
+			get: async function(){
+				return await http.get(this.url);
+			}
+		},
+		list: {
+			url: `${config.API_URL}/system/menu/list`,
+			name: "获取菜单",
+			get: async function(){
+				return await http.get(this.url);
+			}
+		}
+	},
+	dic: {
+		tree: {
+			url: `${config.API_URL}/system/dic/tree`,
+			name: "获取字典树",
+			get: async function(){
+				return await http.get(this.url);
+			}
+		},
+		list: {
+			url: `${config.API_URL}/system/dic/list`,
+			name: "字典明细",
+			get: async function(params){
+				return await http.get(this.url, params);
+			}
+		},
+		get: {
+			url: `${config.API_URL}/system/dic/get`,
+			name: "获取字典数据",
+			get: async function(params){
+				return await http.get(this.url, params);
+			}
+		}
+	},
+	role: {
+		list: {
+			url: `${config.API_URL_2}/system/role/list`,
+			name: "获取角色列表",
+			get: async function(params){
+				return await http.get(this.url, params);
+			}
+		},
+		save: {
+			url: `${config.API_URL_2}/system/role/save`,
+			name: "获取角色列表",
+			post: async function(params){
+				return await http.post(this.url, params);
+			}
+		},
+		del: {
+			url: `${config.API_URL_2}/system/role/deleteById`,
+			name: "单个删除",
+			post: async function(params){
+				return await http.post(this.url, params);
+			}
+		},
+		batchDel:{
+			url: `${config.API_URL_2}/system/role/batchDel`,
+			name: "批量删除",
+			post: async function(params){
+				return await http.post(this.url, params);
+			}
+		}
+	},
+	dept: {
+		list: {
+			url: `${config.API_URL_2}/system/dept/list`,
+			name: "获取部门列表",
+			get: async function(params){
+				return await http.get(this.url, params);
+			}
+		},
+		save: {
+			url: `${config.API_URL_2}/system/dept/add`,
+			name: "新增部门列表",
+			post: async function(params){
+				return await http.post(this.url, params);
+			}
+		},
+		del: {
+			url: `${config.API_URL_2}/system/dept/deleteById`,
+			name: "删除",
+			post: async function(params){
+				return await http.post(this.url, params);
+			}
+		},
+		batchDel: {
+			url: `${config.API_URL_2}/system/dept/batchDel`,
+			name: "删除",
+			post: async function(params){
+				return await http.post(this.url, params);
+			}
+		},
+		edit: {
+			url: `${config.API_URL_2}/system/dept/edit`,
+			name: "新增部门列表",
+			post: async function(params){
+				return await http.post(this.url, params);
+			}
+		}
+	},
+	user: {
+		list: {
+			url: `${config.API_URL}/system/user/list`,
+			name: "获取用户列表",
+			get: async function(params){
+				return await http.get(this.url, params);
+			}
+		}
+	},
+	app: {
+		list: {
+			url: `${config.API_URL}/system/app/list`,
+			name: "应用列表",
+			get: async function(){
+				return await http.get(this.url);
+			}
+		}
+	},
+	log: {
+		list: {
+			url: `${config.API_URL}/system/log/list`,
+			name: "日志列表",
+			get: async function(params){
+				return await http.get(this.url, params);
+			}
+		}
+	},
+	table: {
+		list: {
+			url: `${config.API_URL}/system/table/list`,
+			name: "表格列管理列表",
+			get: async function(params){
+				return await http.get(this.url, params);
+			}
+		},
+		info: {
+			url: `${config.API_URL}/system/table/info`,
+			name: "表格列管理详情",
+			get: async function(params){
+				return await http.get(this.url, params);
+			}
+		}
+	},
+	tasks: {
+		list: {
+			url: `${config.API_URL}/system/tasks/list`,
+			name: "系统任务管理",
+			get: async function(params){
+				return await http.get(this.url, params);
+			}
+		}
+	}
+}
---
+++ src/views/setting/role/save.vue
@@ -0,0 +1,101 @@
+<template>
+	<el-dialog :title="titleMap[mode]" v-model="visible" :width="500" destroy-on-close @closed="$emit('closed')">
+		<el-form :model="form" :rules="rules" :disabled="mode=='show'" ref="dialogForm" label-width="100px" label-position="left">
+			<el-form-item label="角色名称" prop="label">
+				<el-input v-model="form.roleName" clearable></el-input>
+			</el-form-item>
+			<el-form-item label="角色别名" prop="alias">
+				<el-input v-model="form.roleAlias" clearable></el-input>
+			</el-form-item>
+			<el-form-item label="排序" prop="sort">
+				<el-input-number v-model="form.roleSort" controls-position="right" :min="0" style="width: 100%;"></el-input-number>
+			</el-form-item>
+			<el-form-item label="是否有效" prop="status">
+				<el-switch v-model="form.status" active-text="启用" inactive-text="停用" :active-value="0" :inactive-value="1"></el-switch>
+			</el-form-item>
+		</el-form>
+		<template #footer>
+			<el-button @click="visible=false" >取 消</el-button>
+			<el-button v-if="mode!='show'" type="primary" :loading="isSaveing" @click="submit()">保 存</el-button>
+		</template>
+	</el-dialog>
+</template>
+
+<script>
+	export default {
+		emits: ['success', 'closed'],
+		data() {
+			return {
+				mode: "add",
+				titleMap: {
+					add: '新增',
+					edit: '编辑',
+					show: '查看'
+				},
+				visible: false,
+				isSaveing: false,
+				//表单数据
+				form: {
+					roleId:"",
+					roleName: "",
+					roleAlias: "",
+					roleSort: 0,
+					status: 1,
+				},
+				//验证规则
+				rules: {
+					roleSort: [
+						{required: true, message: '请输入排序', trigger: 'change'}
+					],
+					roleName: [
+						{required: true, message: '请输入角色名称'}
+					],
+					roleAlias: [
+						{required: true, message: '请输入角色别名'}
+					]
+				}
+			}
+		},
+		mounted() {
+
+		},
+		methods: {
+			//显示
+			open(mode='add'){
+				this.mode = mode;
+				this.visible = true;
+				return this
+			},
+			//表单提交方法
+			submit(){
+				this.$refs.dialogForm.validate(async (valid) => {
+					if (valid) {
+						this.isSaveing = true;
+						var res = await this.$API.system.role.save.post(this.form);
+						this.isSaveing = false;
+						if(res.code == 200){
+							this.$emit('success', this.form, this.mode)
+							this.visible = false;
+							this.$message.success("操作成功")
+						}else{
+							this.$alert(res.message, "提示", {type: 'error'})
+						}
+					}
+				})
+			},
+			//表单注入数据
+			setData(data){
+				this.form.roleId = data.roleId
+				this.form.roleName = data.roleName
+				this.form.roleAlias = data.roleAlias
+				this.form.roleSort = data.roleSort
+				this.form.status = data.status
+				//可以和上面一样单个注入，也可以像下面一样直接合并进去
+				//Object.assign(this.form, data)
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
